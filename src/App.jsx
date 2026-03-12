ae371887199e374c4d542d01b304b1760bd29e33
Minhajul43 <minhajul255685islam@gmail.com>
2026-03-12 12:26:19 +0600

    explore JSX and props basic

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,61 @@ function App() {
     <>
 
       <h1>Core Concept of React!</h1>
+      <Student></Student>
+      <Student></Student>
+      <Person></Person>
+      <Developer name="Minhajul" tech="JavaScript"></Developer>
+      <Developer name="Juel" tech="Python"></Developer>
+      <Developer name="Tahmeed" tech="Java"></Developer>
       
       
     </>
   )
 }
 
+function Developer(props) {
+  return (
+    <div
+      style={{
+        border: '2px solid blue',
+        margin: '10px',
+        padding: '10px',
+        borderRadius:'10px'
+    }}>
+      <h3>Developer:{props.name} </h3>
+      <p>Technology:{props.tech} </p>
+    </div>
+  )
+}
+
+const personStyle = {
+  color: 'red',
+  backgroundColor: 'white',
+  border: '1px solid green',
+  borderRadius:'10px'
+  
+}
+
 function Person() {
        const age=23;
        const name='Minhajul';
         return(
-         <p>This {name} is {age} years old.</p>
+         <p style={personStyle}>This {name} is {age} years old.</p>
         )
 }
  
 function Sports() {
   return (
     <p> I want to play sports.</p>
+  )
+}
+
+function Student() {
+  return (
+    <div className='student'>
+      <p>Name: </p>
+      <p>Dept: </p>
+    </div>
   )
 }
 
